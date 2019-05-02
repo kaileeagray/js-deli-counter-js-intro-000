7a70c472a83aac827996eb3685b727ba2f08ab45
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 17:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty.";
   } else {
+    for (var i = 0, i < line.length; i++) {
+      
+    }
     return `The line is currently: ${lineList}.`;
   }
 }
