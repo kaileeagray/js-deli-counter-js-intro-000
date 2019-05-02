6f142ae36c0b29056d6ab3da22c6300db1f26076
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 17:34:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(line) {
 
 function currentLine(line) {
   if (line.length === 0) {
-    return "There is nobody waiting to be served!";
+    return "The line is currently empty.";
   } else {
     return `Currently serving ${line.shift()}.`;
   }
