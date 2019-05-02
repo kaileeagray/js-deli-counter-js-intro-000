34d82bec6bd201592e7868b1bc0900250dfa5559
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 17:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line, person) {
   line.push(person);
-  return `Welcome, ${person}. You are number ${line} in line.`
+  return `Welcome, ${person}. You are number ${line.indexOf(person)+1} in line.`
 }
